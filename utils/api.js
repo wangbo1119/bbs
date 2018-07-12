4aae57f4f4abfaeaf55a879587d6ebad87a49140
wangbo <1018484984@qq.com>
2018-07-12 15:47:40 +0800

    MD

--- utils/api.js
+++ utils/api.js
@@ -51,37 +51,9 @@ const getQuestion = (params) => {
 const login = (params) => {
   wxRequest(params, `${apiURL}/session`);
 };
-const getPlaceInfoByID = (params) => {
-  wxRequest(params, `${apiURL}/destination/place/${params.query.type}/${params.query.id}/`);
-};
-const getPlacePOIByID = (params) => {
-  wxRequest(params, `${apiURL}/destination/place/${params.query.type}/${params.query.id}/pois/${params.query.poiType}/`);
-};
-const getTripInfoByID = (params) => {
-  wxRequest(params, `${apiURL}/trips/${params.query.tripId}/waypoints/`);
-};
-const getPlaceTripByID = (params) => {
-  wxRequest(params, `${apiURL}/destination/place/${params.query.type}/${params.query.id}/trips/`);
-};
-const getUserInfoByID = (params) => {
-  wxRequest(params, `${apiURL}/users/${params.query.userId}/v2`);
-};
-const getWaypointInfoByID = (params) => {
-  wxRequest(params, `${apiURL}/trips/${params.query.tripId}/waypoints/${params.query.waypointId}/`);
-};
-const getWaypointReplyByID = (params) => {
-  wxRequest(params, `${apiURL}/trips/${params.query.tripId}/waypoints/${params.query.waypointId}/replies/`);
-};
 
 module.exports = {
   getForum,
   getQuestion,
   login,
-  getPlaceInfoByID,
-  getPlacePOIByID,
-  getTripInfoByID,
-  getPlaceTripByID,
-  getUserInfoByID,
-  getWaypointInfoByID,
-  getWaypointReplyByID,
 };
